468e5ba3b164e1cd5b8f3bb924180504aabc9113
Vaernion <vaporz@gmail.com>
2022-06-15 15:41:52 +0200

    whitespace for readabiltiy

--- MovingCompanyAPI/Models/Order.cs
+++ MovingCompanyAPI/Models/Order.cs
@@ -7,17 +7,26 @@ public class Order
     [Key]
     public int Id { get; set; }
     public bool IsDone { get; set; }
+
     /// <summary>Date when order was last updated</summary>
     public DateTime UpdateDate { get; set; }
+
     /// <summary>Date when order should be performed</summary>
     [Required]
     public DateTime OrderDate { get; set; }
+
     [Required]
     public string AddressFrom { get; set; } = string.Empty;
+
     [Required]
     public string AddressTo { get; set; } = string.Empty;
+
     public string Note { get; set; } = string.Empty;
+
+    [Required]
     public OrderServices Services { get; set; } = new OrderServices();
+
+    [Required]
     public Customer Customer { get; set; } = new Customer();
 }
 
@@ -32,9 +41,11 @@ public class Customer
 {
     [Required]
     public string Name { get; set; } = string.Empty;
+
     [Required]
     [Phone]
     public string Phone { get; set; } = string.Empty;
+
     [Required]
     [EmailAddress]
     public string Email { get; set; } = string.Empty;
